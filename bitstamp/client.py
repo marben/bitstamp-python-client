4842b10336460f6778678dc430e20ac886fd405a
Martin Benes <martinbenesh@gmail.com>
2013-11-22 14:51:50 +0100

    limit number of requests to 600 in last 10 minutes

--- bitstamp/client.py
+++ bitstamp/client.py
@@ -1,10 +1,10 @@
-__author__ = 'kmadac'
-
 import requests
 import time
 import hmac
 import hashlib
 import sys
+import collections
+import time
 
 class public():
     def __init__(self, rootUrl=None, proxydict=None):
@@ -14,28 +14,25 @@ class public():
         else:
             self.rootUrl = "https://www.bitstamp.net"
 
-        self.session = requests.Session()
-
     def ticker(self):
         """
-        Return dictionary
+        Return ticker output
         """
-        r = self.session.get(self.rootUrl + "/api/ticker/", proxies=self.proxydict)
+        r = limiter.get(self.rootUrl + "/api/ticker/", proxies=self.proxydict)
         if r.status_code == 200:
-            return r.text
+            return r.content
         else:
             r.raise_for_status()
 
     def order_book(self, group=True):
         """
-        Returns JSON dictionary with "bids" and "asks".
-        Each is a list of open orders and each order is represented as a list of price and amount.
+        Returns "bids" and "asks".
         """
         params = {'group': group}
 
-        r = self.session.get(self.rootUrl + "/api/order_book/", params=params, proxies=self.proxydict)
+        r = limiter.get(self.rootUrl + "/api/order_book/", params=params, proxies=self.proxydict)
         if r.status_code == 200:
-            return r.json()
+            return r.content
         else:
             r.raise_for_status()
 
@@ -45,30 +42,29 @@ class public():
         """
         params = {'timedelta': timedelta_secs}
 
-        r = self.session.get(self.rootUrl + "/api/transactions/", params=params, proxies=self.proxydict)
+        r = limiter.get(self.rootUrl + "/api/transactions/", params=params, proxies=self.proxydict)
         if r.status_code == 200:
-            return r.json()
+            return r.content
         else:
             r.raise_for_status()
 
     def bitinstant_reserves(self):
         """
-        Returns simple dictionary {'usd': 'Bitinstant USD reserves'}
+        Returns {'usd': 'Bitinstant USD reserves'}
         """
-        r = self.session.get(self.rootUrl + "/api/bitinstant/", proxies=self.proxydict)
+        r = limiter.get(self.rootUrl + "/api/bitinstant/", proxies=self.proxydict)
         if r.status_code == 200:
-            return r.json()
+            return r.content
         else:
             r.raise_for_status()
 
     def conversion_rate_usd_eur(self):
         """
-        Returns simple dictionary
-        {'buy': 'buy conversion rate', 'sell': 'sell conversion rate'}
+        Returns {'buy': 'buy conversion rate', 'sell': 'sell conversion rate'}
         """
-        r = self.session.get(self.rootUrl + "/api/eur_usd/", proxies=self.proxydict)
+        r = limiter.get(self.rootUrl + "/api/eur_usd/", proxies=self.proxydict)
         if r.status_code == 200:
-            return r.json()
+            return r.content
         else:
             r.raise_for_status()
 
@@ -98,7 +94,7 @@ class trading():
 
     def account_balance(self):
         """
-        Returns dictionary:
+        Returns
         {u'btc_reserved': u'0',
          u'fee': u'0.5000',
          u'btc_available': u'2.30856098',
@@ -108,18 +104,18 @@ class trading():
          u'usd_available': u'114.64'}
         """
         params = self.get_params()
-        r = self.session.post(self.rootUrl + "/api/balance/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/balance/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
 
     def user_transactions(self, offset=0, limit=100, descending=True):
         """
-        Returns descending list of transactions. Every transaction (dictionary) contains
+        Returns descending list of transactions. Every transaction contains
         {u'usd': u'-39.25',
          u'datetime': u'2013-03-26 18:49:13',
          u'fee': u'0.20', u'btc': u'0.50000000',
@@ -134,26 +130,26 @@ class trading():
         else:
             params['sort'] = "asc"
 
-        r = self.session.post(self.rootUrl + "/api/user_transactions/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/user_transactions/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
 
     def open_orders(self):
         """
-        Returns JSON list of open orders. Each order is represented as dictionary:
+        Returns list of open orders.
         """
         params = self.get_params()
-        r = self.session.post(self.rootUrl + "/api/open_orders/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/open_orders/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
 
@@ -165,9 +161,9 @@ class trading():
         """
         params = self.get_params()
         params['id'] = order_id
-        r = self.session.post(self.rootUrl + "/api/cancel_order/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/cancel_order/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
-            if r.text == u'true':
+            if r.content == u'true':
                 return True
             else:
                 return False, r.json()['error']
@@ -182,12 +178,12 @@ class trading():
         params['amount'] = amount
         params['price'] = price
 
-        r = self.session.post(self.rootUrl + "/api/buy/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/buy/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
 
@@ -199,59 +195,59 @@ class trading():
         params['amount'] = amount
         params['price'] = price
 
-        r = self.session.post(self.rootUrl + "/api/sell/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/sell/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
             
     def check_bitstamp_code(self, code):
         """
-        Returns JSON dictionary containing USD and BTC amount included in given bitstamp code.
+        Returns USD and BTC amount included in given bitstamp code.
         """
         params = self.get_params()
         params['code'] = code
-        r = self.session.post(self.rootUrl + "/api/check_code/", data=params,
+        r = limiter.post(self.rootUrl + "/api/check_code/", data=params,
                           proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
             
     def redeem_bitstamp_code(self, code):
         """
-        Returns JSON dictionary containing USD and BTC amount added to user's account.
+        Returns USD and BTC amount added to user's account.
         """
         params = self.get_params()
         params['code'] = code
-        r = self.session.post(self.rootUrl + "/api/redeem_code/", data=params,
+        r = limiter.post(self.rootUrl + "/api/redeem_code/", data=params,
                           proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
             
     def withdrawal_requests(self):
         """
-        Returns list of withdrawal requests. Each request is represented as dictionary
+        Returns list of withdrawal requests.
         """
         params = self.get_params()
-        r = self.session.post(self.rootUrl + "/api/withdrawal_requests/", data=params,
+        r = limiter.post(self.rootUrl + "/api/withdrawal_requests/", data=params,
                           proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()            
 
@@ -263,9 +259,9 @@ class trading():
         params['amount'] = amount
         params['address'] = address
 
-        r = self.session.post(self.rootUrl + "/api/bitcoin_withdrawal/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/bitcoin_withdrawal/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
-            if r.text == u'true':
+            if r.content == u'true':
                 return True
             else:
                 return False, r.json()['error']
@@ -277,31 +273,31 @@ class trading():
         Returns bitcoin deposit address as unicode string
         """
         params = self.get_params()
-        r = self.session.post(self.rootUrl + "/api/bitcoin_deposit_address/", data=params,
+        r = limiter.post(self.rootUrl + "/api/bitcoin_deposit_address/", data=params,
                           proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.text
+                return r.content
         else:
             r.raise_for_status()
 
     def unconfirmed_bitcoin_deposits(self):
         """
-        Returns JSON list of unconfirmed bitcoin transactions. Each transaction is represented as dictionary:
+        Returns unconfirmed bitcoin transactions. Each transaction is represented:
         amount - bitcoin amount
         address - deposit address used
         confirmations - number of confirmations
         """
         params = self.get_params()
-        r = self.session.post(self.rootUrl + "/api/unconfirmed_btc/", data=params,
+        r = limiter.post(self.rootUrl + "/api/unconfirmed_btc/", data=params,
                           proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.json()
+                return r.content
         else:
             r.raise_for_status()
             
@@ -314,9 +310,9 @@ class trading():
         params['address'] = address
         params['currency'] = currency
 
-        r = self.session.post(self.rootUrl + "/api/ripple_withdrawal/", data=params, proxies=self.proxydict)
+        r = limiter.post(self.rootUrl + "/api/ripple_withdrawal/", data=params, proxies=self.proxydict)
         if r.status_code == 200:
-            if r.text == u'true':
+            if r.content == u'true':
                 return True
             else:
                 return False, r.json()['error']
@@ -328,12 +324,70 @@ class trading():
         Returns ripple deposit address as unicode string
         """
         params = self.get_params()
-        r = self.session.post(self.rootUrl + "/api/ripple_address/", data=params,
+        r = limiter.post(self.rootUrl + "/api/ripple_address/", data=params,
                           proxies=self.proxydict)
         if r.status_code == 200:
             if 'error' in r.json():
                 return False, r.json()['error']
             else:
-                return r.text
+                return r.content
         else:
             r.raise_for_status()
+
+class LimiterException(Exception):
+    def __init__(self, msg, timeToWait):
+        Exception.__init__(self, msg)
+        self.timeToWait = timeToWait
+
+class Limiter():
+    def __init__(self, limit=True):
+        # as specified by bitstamp api:
+        self.requestsLimitNo = 600  # we limit to 600 requests
+        self.requestsLimitInterval = 600 # 600s == 10 minutes
+        # enable/disable limiting boolean var:
+        self.limit = limit
+        # some internals:
+        self.session = requests.Session()
+        self.dequeue = collections.deque(maxlen=self.requestsLimitNo)
+
+    def intervalWithinLimit(self, timeA, timeB):
+        ''' returns True if inteval fits within the limit(if the interval is > 10min). returns False otherwise '''
+        if abs(timeA - timeB) > self.requestsLimitInterval:
+            return True
+        else:
+            return False
+
+    def _checkLimit(self):
+        # if limiting is disabled, we just return
+        if not self.limit:
+            return
+
+        currTime = time.time()
+        # if there is still space left(number of requests<=600), we just add current time
+        if len(self.dequeue) < self.dequeue.maxlen:
+            self.dequeue.append(currTime)
+            return
+
+        # else, we need to check, whether we didn't make too many requests
+        lastTimestamp = self.dequeue.popleft()
+
+        # if the interval is > 600s, we pop it from left, add curr time and return as success
+        if self.intervalWithinLimit(currTime, lastTimestamp):
+            self.dequeue.append(currTime)
+            return
+
+        # else we don't fit within the limit, so we throw exception
+        self.dequeue.appendleft(lastTimestamp)
+        timeToWait = self.requestsLimitInterval - (currTime - lastTimestamp)
+        raise LimiterException("Sent more than " + str(self.requestsLimitNo) + " requests in last:" + str(self.requestsLimitInterval) + " seconds. Wait for: " + str(timeToWait) + "seconds", timeToWait)
+
+    def get(self, *args, **kwargs):
+        self._checkLimit()
+        return self.session.get(*args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        self._checkLimit()
+        return self.session.post(*args, **kwargs)
+
+limiter = Limiter(limit=True)
+limiter.limit = True  # set to False from your program to disable limiting
